e97b4d62a07fded4252e7c4e49f2ee67476f8bb0
Maggie <maggie@pop-os.localdomain>
2022-05-12 00:51:56 -0500

    first commit

---
+++ init.lua
@@ -0,0 +1,42 @@
+-- los plugins
+require('plugins')
+
+-- configuracion y atajos
+require('settings')
+require('keymaps')
+
+-- conf de los plugins
+require('staline-config')
+require('treesitter-config')
+require('bufferline-config')
+require('tree-config')
+require('telescope-config')
+require('cmp-config')
+require('lsp-config')
+require('lenguajes')
+require('lspsaga-config')
+require('autopairs-config')
+require('whichkeys-config')
+require('toggleterm-config')
+require('blankline-config')
+require('gitsigns-config')
+require('dashboard-config')
+require('rnvimr-config')
+require('trouble-config')
+require('nulls-ls-config')
+require('reach-config')
+require('comment-config')
+require('rename-config')
+require('hop-vim')
+require('refactoring-nvim')
+require('nvim-spectre')
+
+
+--moon
+--vim.cmd[[colorscheme blue-moon]]
+
+--catppuccin
+--vim.cmd[[colorscheme catppuccin]]
+
+--tokyonight
+vim.cmd[[colorscheme tokyonight]]
---
+++ lua/plugins/init.lua
@@ -0,0 +1,151 @@
+return require('packer').startup(function()
+  -- My plugins here
+  -- Packer can manage itself
+  use 'folke/tokyonight.nvim'
+
+  --tema nord
+  use 'shaunsingh/nord.nvim'
+  --tema 2
+  use 'tiagovla/tokyodark.nvim'
+  --tema 3
+  use "rafamadriz/neon"
+  --tema 4
+  use 'kyazdani42/blue-moon'
+
+	--la linea de abajo
+	use {
+    use 'tamton-aquib/staline.nvim',
+	  requires = {'kyazdani42/nvim-web-devicons', opt = true}
+	}
+	
+  -- la sintaxis
+  use {'nvim-treesitter/nvim-treesitter', run = ":TSUpdate"}
+
+  --los buffers
+  use {'akinsho/bufferline.nvim', requires = 'kyazdani42/nvim-web-devicons'}
+
+  --navegador de archivos
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = {
+      'kyazdani42/nvim-web-devicons', -- optional, for file icon
+    },
+    config = function() require'nvim-tree'.setup {} end
+  }
+
+  --telescope
+  use {
+  'nvim-telescope/telescope.nvim',
+  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  --LSP y autocompletado
+  require("nvim-lsp-installer").setup {}
+  require'lspconfig'.pyright.setup{}
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-vsnip'
+  use 'hrsh7th/vim-vsnip'
+  use 'williamboman/nvim-lsp-installer'
+  use 'neovim/nvim-lspconfig'
+  use { 'tami5/lspsaga.nvim' }
+  use "jose-elias-alvarez/null-ls.nvim"
+
+  -- para Hob
+  use {
+  'phaazon/hop.nvim',
+  branch = 'v1', -- optional but strongly recommended
+  config = function()
+    -- you can configure Hop the way you like here; see :h hop-config
+    require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+  end
+}
+
+  --para refactoring
+  use {
+    "ThePrimeagen/refactoring.nvim",
+    requires = {
+        {"nvim-lua/plenary.nvim"},
+        {"nvim-treesitter/nvim-treesitter"}
+    }
+}
+
+  --Spectre
+  use 'windwp/nvim-spectre'
+  use "nvim-lua/plenary.nvim"
+
+  --para java
+  use 'mfussenegger/nvim-jdtls'
+
+  --autopairs
+  use {'windwp/nvim-autopairs'}
+
+  --which-key
+  use {'folke/which-key.nvim'}
+
+  --iconos en cmp
+  use 'onsails/lspkind-nvim' 
+  
+  --terminal
+  use {"akinsho/toggleterm.nvim"}
+
+
+  --indentado
+  use "lukas-reineke/indent-blankline.nvim"
+
+  --ver qué se modificó
+  use {
+    'lewis6991/gitsigns.nvim',
+    requires = {
+      'nvim-lua/plenary.nvim'
+    },
+    config = function()
+      require('gitsigns').setup{current_line_blame = true}
+    end
+  }
+  --pantalla de inicio
+  use 'glepnir/dashboard-nvim'
+		
+  --ranger
+  use 'kevinhwang91/rnvimr'
+		
+    --problemas
+  use {
+    "folke/trouble.nvim",
+    requires = "kyazdani42/nvim-web-devicons",
+    config = function()
+      require("trouble").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end
+  }
+		
+--tema chido
+ use({
+	 "catppuccin/nvim",
+	 as = "catppuccin"
+ })
+
+ -- buffers
+ use 'toppair/reach.nvim'
+
+ -- scroll en pantalla
+ use 'karb94/neoscroll.nvim'
+
+ -- para comentar lineas
+ use "terrortylor/nvim-comment"
+
+ -- renombrar
+ use {
+    'filipdutescu/renamer.nvim',
+    branch = 'master',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+-- symbols-outline
+use 'simrat39/symbols-outline.nvim'
+
+-- fin de packer  
+end)
